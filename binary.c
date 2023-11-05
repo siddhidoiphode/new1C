daa6a5bc16c3bd24b4ff362061782b6500f04a5a
siddhidoiphode <141642695+siddhidoiphode@users.noreply.github.com>
2023-11-05 21:33:03 +0530

    binary.c
    binary search

--- binary.c
+++ binary.c
@@ -1,28 +1,30 @@
+// binary search
 #include <stdio.h>
-int main(void) 
+int main (void)
 {
-int size,i,flag=0;
-int lis[15],sorted,j;
-printf("Enter size of array: \n");
+  int size,i,flag=0,f=0;
+  int lis[15],search,low=0,high=size-1,mid;
+  printf("Enter size of array: \n");
   scanf("%d",&size);
-printf("\n Enter elements : \n");
-for(i=0;i<size;i++)
-  scanf("%d",&lis[i]);
+  printf("\n Enter elements : \n\n");
 
-for(i=1;i<size;i++)
-{
-   sorted=lis[i];
-  for(j=i-1;j>=0&& lis[j]>=sorted;j--)
-    {
-      lis[j+1]=lis[j];
-    }
-    lis[j+1]=sorted;
-}
-  
-printf("list after sorting:");
   for(i=0;i<size;i++)
-     printf("\n%d",lis[i]);
+    scanf("%d",&lis[i]);
+  printf("\n Enter element to be searched: ");
+  scanf("%d",&search);
+  low=0,high=size-1;
+  while(low<=high)
+  {  mid=(low+high)/2;
+  if(search==lis[mid])
+  { printf("\n Element found at %d position",mid+1);
+  flag=1;
+  break;
+  }
+   else if(search<lis[mid])
+     high=mid-1;
+  else 
+    low=mid+1;
+  }
 
-  
   return 0;
-}
+  }
